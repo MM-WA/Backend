7df68e57b4040b0b6f9c056f903bc3b9ee95d334
MM-WA <144456091+MM-WA@users.noreply.github.com>
2025-04-10 15:18:35 +0500

    Added utils.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js"
 
-dotenv.config({ path: "./env" });
+dotenv.config({ path: "./.env" });
+
 connectDB()
- 
+  .then(() => {
+      const port = process.env.PORT || 8000;
+      app.on("error", (error) => {
+        console.log("Express app error: ", error) 
+      });
+
+      const server = app.listen(port, () => {
+        console.log("Server is running at port: ", process.env.PORT);
+      });
+
+      server.on("error", (error) => {
+        console.log("HTTP server error: ", error)
+      })
+
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error: ", error)
+    process.exit(1)
+  });
