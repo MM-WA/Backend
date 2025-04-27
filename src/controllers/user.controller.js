4afe805ba21fcbbfdf273b571bde820956dc5f34
MM-WA <144456091+MM-WA@users.noreply.github.com>
2025-04-27 20:25:05 +0500

    feature - implemented user routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import ApiResponse from "../utils/apiResponse.js";
 import User from "../models/user.model.js";
 import uploadOnCloudinary from "../utils/cloudinary.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -397,7 +398,7 @@ const getUserChannelProfile = asynHandler(async (req, res) => {
         subscribedToCount: 1,
         isSubscribed: 1,
       },
-    },
+    }
   ]);
 
   res
@@ -406,6 +407,56 @@ const getUserChannelProfile = asynHandler(async (req, res) => {
       new ApiResponse(200, "Channel profile fetched successfully", channel[0])
     );
 });
+
+const getWatchHistory = asynHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user?._id)
+      }
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    username: 1,
+                    fullname: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200, "Watch history fetched successfuly", user[0].watchHistory)
+  )
+})
 
 export {
   registerUser,
@@ -418,4 +469,5 @@ export {
   updateAvatar,
   updateCoverImage,
   getUserChannelProfile,
+  getWatchHistory
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   loginUser,
   logoutUser,
   accessTokenRefresher,
+  changePassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateAvatar,
+  updateCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import upload from "../middlewares/multer.middleware.js";
 import verifyJWT from "../middlewares/auth.middleware.js";
@@ -28,6 +35,13 @@ router.route("/login").post(loginUser);
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/refreshToken").post(accessTokenRefresher);
+router.route("/refreshToken").post(accessTokenRefresher);//*** 
+router.route("/changePassword").post(verifyJWT, changePassword)
+router.route("/currentUser").get(verifyJWT, getCurrentUser)
+router.route("/updateAccount").patch(verifyJWT, updateAccountDetails)
+router.route("/updateAvatar").patch(verifyJWT, upload.single("avatar"), updateAvatar)
+router.route("/updateCoverImage").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
+router.route("/channelProfile/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/watchHistory").get(verifyJWT, getWatchHistory)
 
 export default router;
