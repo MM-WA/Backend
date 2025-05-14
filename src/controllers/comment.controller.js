7589684742d52c3a053cbfc71542020d91c787dd
MM-WA <144456091+MM-WA@users.noreply.github.com>
2025-05-14 11:17:51 +0500

    feature - working on video comments controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,15 +1,24 @@
-import mongoose from "mongoose";
-import { Comment } from "../models/comment.model.js";
+import Comment from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
-  const { videoId } = req.params;
-  const { page = 1, limit = 10 } = req.query;
+  try {
+    const { videoId } = req.params;
+    const { page = 1, limit = 10 } = req.query;
 
-  
+    if (!videoId) {
+      throw new ApiError(400, "Video id required");
+    }
+
+    const aggregateQuery = await Comment.aggregate([]);
+
+  } catch (error) {
+    if (error instanceof ApiError) throw error;
+    throw new ApiError(500, "Something went wrong while fetching comments");
+  }
 });
 
 const addComment = asyncHandler(async (req, res) => {
